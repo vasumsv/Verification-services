328093ef6dc2fbe45cd90bab3d0a149a088db3e2
vasumsv <quickproject1312@gmail.com>
2025-09-22 02:10:33 +0530

    url updated

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // CORS middleware for development
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://verification-services-phi.vercel.app/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   
